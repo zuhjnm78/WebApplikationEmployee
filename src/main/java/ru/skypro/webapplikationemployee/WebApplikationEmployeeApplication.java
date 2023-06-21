0e0a973723670971cba81a329f5d771a99f8f3a0
oleghenkel <oleg.henkel75@gmail.com>
2023-06-21 17:18:46 +0200

    HomeWork List

---
+++ src/main/java/ru/skypro/webapplikationemployee/WebApplikationEmployeeApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.webapplikationemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplikationEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplikationEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/webapplikationemployee/WebApplikationEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.webapplikationemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplikationEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
